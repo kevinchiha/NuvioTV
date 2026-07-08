bf07f31bc16857af86f5c9f2229a3c73b52345cc
Kevin Chiha <kevin.chiha@gmail.com>
2026-07-08 11:22:14 +0300

    chore(app): bump version to 0.8.16-beta (versionCode 1039)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -115,8 +115,8 @@ android {
         applicationId = "tv.kevbox"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1038
-        versionName = "0.8.9-beta"
+        versionCode = 1039
+        versionName = "0.8.16-beta"
 
         buildConfigField("String", "PARENTAL_GUIDE_API_URL", "\"${localProperties.getProperty("PARENTAL_GUIDE_API_URL", "")}\"")
         buildConfigField("String", "INTRODB_API_URL", "\"${localProperties.getProperty("INTRODB_API_URL", "")}\"")
